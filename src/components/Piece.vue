e83ef3191fb59f635429b598736fb6a42503edad
Takodakko <monchichi@Monchichis-Air.lan>
2024-05-03 11:35:23 -0400

    added pawns

---
+++ src/components/Piece.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+//import { ref } from 'vue'
+
+const props = defineProps({
+  color: {type: String, required: true},
+  type: {type: String, required: true}
+});
+const emit = defineEmits(['chose-piece']);
+const pieceColor = `bg-${props.color}-500`;
+const pieceName = `${props.color} ${props.type}`;
+const choosePiece = function() {
+  emit('chose-piece');
+}
+
+</script>
+
+<template>
+  <div :class=pieceColor @click="choosePiece">
+    {{pieceName}}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/Square.vue
+++ src/components/Square.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import King from './King.vue'
+import { computed, ref } from 'vue'
+import Piece from './Piece.vue'
 
 // defineProps<{ tileNumber: string, piecePositions: Object }>()
 const props = defineProps({
@@ -8,12 +8,18 @@ const props = defineProps({
   piecePositions: {type: Object, required: true},
 
 });
+const emit = defineEmits(['toggle-active-piece']);
 const { tileNumber, piecePositions } = props;
+const isChosen = ref(false);
 
 const here = piecePositions[tileNumber];
 const containsRedKing = computed(() => here === 'redKing');
 const containsCyanKing = computed(() => here === 'cyanKing');
-//const count = ref(0)
+const containsRedPawn = computed(() => here === 'redPawn');
+const containsCyanPawn = computed(() => here === 'cyanPawn');
+function chosePiece() {
+  isChosen.value = !isChosen.value
+}
 
 const emptySquareCss = 'bg-white border-solid border-black border-2 max-w-xs min-w-20 max-h-14 min-h-20';
 </script>
@@ -21,8 +27,10 @@ const emptySquareCss = 'bg-white border-solid border-black border-2 max-w-xs min
 <template>
   <div :class="emptySquareCss">
     {{tileNumber}}
-    <King v-if="containsCyanKing" color="pink"/>
-    <King v-if="containsRedKing" color="red"/>
+    <Piece v-if="containsRedKing" color="red" type="king" @chose-piece="chosePiece"/>
+    <Piece v-if="containsCyanKing" color="cyan" type="king"/>
+    <Piece v-if="containsRedPawn" color="red" type="pawn"/>
+    <Piece v-if="containsCyanPawn" color="cyan" type="pawn"/>
   </div>
 </template>
 
